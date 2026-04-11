69d61e5b7af43f0896a14505feacce547e2ce812
Andy Weaver <andrewayersweaver@gmail.com>
2026-04-11 18:23:05 -0400

    feat(app): add bootstrap gating integration slice

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { orchestrateGraphBootstrap } from "./graph/bootstrap.orchestrator";
+import { fixtureGraphDataSource } from "./graph/fixture-data-source.adapter";
+import { graphValidator } from "./graph/validator";
+import "./index.css";
+
+const runBootstrap = () =>
+  orchestrateGraphBootstrap({
+    dataSource: fixtureGraphDataSource,
+    validator: graphValidator,
+  });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App runBootstrap={runBootstrap} />
   </StrictMode>,
 )
